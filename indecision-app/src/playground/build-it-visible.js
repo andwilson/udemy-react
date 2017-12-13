bf335957f2c04fd1ef989dfe3a920d251aaf30b4
Andrew Wilson <anddwilson@gmail.com>
2017-12-13 02:08:26 +0100

    More practice refactoring apps using reusable components

--- indecision-app/src/playground/build-it-visible.js
+++ indecision-app/src/playground/build-it-visible.js
@@ -1,32 +1,29 @@
-const app = {
-	title: 'Visibility Toggle',
-	details: 'Hey. These are some details you can now see!',
-	buttonName: "Show details"
-};
-
-const appRoot = document.getElementById('app');
-
-let buttonState = false;
-const onDetails = (e) => {
-	if (buttonState) {
-		app.buttonName = "Show Details";
-		buttonState = false;
-	} else {
-		app.buttonName = "Hide Details";
-		buttonState = true;
-	};
-	render();
-};
-
-const render = () => {
-	const template = (
-		<div>
-			<h1>{app.title}</h1>
-			<button onClick={onDetails}>{app.buttonName}</button>
-			{buttonState && <p>{app.details}</p>}
-		</div>
-	);
-	ReactDOM.render(template, appRoot);
+class VisibilityToggle extends React.Component {
+	constructor(props) {
+		super(props);
+		this.handleToggleVisibility = this.handleToggleVisibility.bind(this);
+		this.state = {
+			visibility: false
+		}
+	}
+	handleToggleVisibility() {
+		this.setState((prevState) => {
+			return {visibility: !prevState.visibility};
+		});
+	}
+	render() {
+		return (
+			<div>
+				<h1>Visibility Toggle</h1>
+				<button onClick={this.handleToggleVisibility}>
+					{
+						this.state.visibility ? "Hide details" : "Show details"
+					}
+				</button>
+					{this.state.visibility && <p>Hey. These are some details you can now see!</p>}
+			</div>
+		);
+	}
 }
 
-render();
+ReactDOM.render(<VisibilityToggle />, document.getElementById('app'));
